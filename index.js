626bc45e693117a2a171defa4d965e821966b066
lauffenp <lauffenp@gmail.com>
2018-08-20 23:19:26 +0200

    stop push messages temporarily until api is updated

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(bodyParser.text({ type: 'application/json' }));
 
 require('./src/routes')(app);
-require('./src/pushMessageQueue').start();
-require('./src/studyMessageQueue').start();
+// require('./src/pushMessageQueue').start();
+// require('./src/studyMessageQueue').start();
 
 app.listen(REST_PORT, () => {
   console.log(`Rest service ready on port ${REST_PORT}`); // eslint-disable-line no-console
