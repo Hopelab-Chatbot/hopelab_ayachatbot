2173251f6770113383b70e0c3aa231551c7b02fe
lauffenp <lauffenp@gmail.com>
2018-09-11 20:20:07 +0200

    fix event tracking

--- src/events.js
+++ src/events.js
@@ -13,12 +13,11 @@ const logEvent = ({ userId, eventName }) => {
     custom_events: JSON.stringify([{
       _eventName: eventName,
     }]),
-    advertiser_tracking_enabled: 0,
+    advertiser_tracking_enabled: 1,
     application_tracking_enabled: 1,
     extinfo: JSON.stringify(['mb1']),
     page_id: FB_PAGE_ID,
     page_scoped_user_id: userId,
-    app_user_id: userId,
   };
   return apiFetch({ method: 'POST', data, uri});
 };
