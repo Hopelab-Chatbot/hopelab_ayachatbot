7fa475e201f39ad2f5e12e6f2f3f71b67ac6c4ec
lauffenp <lauffenp@gmail.com>
2019-01-25 11:10:12 +0100

    allow event list, tie complete initial to fb_mob_tut_comp

--- src/events.js
+++ src/events.js
@@ -9,16 +9,24 @@ const {
 const fbEventNameFormat = name =>
   name.replace(/[^0-9a-zA-Z_-]/g, '').substr(0,39);
 
-const logEvent = ({ userId, eventName }) => {
-  if (!userId || !eventName) return Promise.resolve();
-  const uri = `${FB_GRAPH_TRUE_ROOT}${FB_APP_ID}${FB_ACTIVITIES_URL}`;
-  const data = {
-    event: 'CUSTOM_APP_EVENTS',
-    custom_events: JSON.stringify([{
+
+const logEvent = ({ userId, eventName, eventList }) => {
+  if (!userId || (!eventName && !eventList)) return Promise.resolve();
+  const custom_events = eventList ?
+    JSON.stringify(eventList.map(name => ({
+      _eventName: fbEventNameFormat(name),
+      _app_user_id: userId,
+      unique_user_id: userId,
+    })))
+    : JSON.stringify([{
       _eventName: fbEventNameFormat(eventName),
       _app_user_id: userId,
       unique_user_id: userId,
-    }]),
+    }]);
+  const uri = `${FB_GRAPH_TRUE_ROOT}${FB_APP_ID}${FB_ACTIVITIES_URL}`;
+  const data = {
+    event: 'CUSTOM_APP_EVENTS',
+    custom_events,
     advertiser_tracking_enabled: 1,
     application_tracking_enabled: 1,
     extinfo: JSON.stringify(['mb1']),
